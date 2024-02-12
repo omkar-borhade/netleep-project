516e344491faea991250c9e722a8174cc8174bb6
omkar-borhade <omkarborhade321952@gmail.com>
2024-02-13 03:06:21 +0530

    Your commit message

--- user/bakend/controllers/orderController.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const orderController = {};
-
-orderController.getOrderPlacePage = (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/orderplace.html'));
-};
-
-module.exports = orderController;
--- user/bakend/middleware/errorHandlerMiddleware.js
+++
@@ -1,8 +0,0 @@
-// errorHandlerMiddleware.js
-const errorHandlerMiddleware = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ success: false, message: 'Something went wrong!' });
-  };
-  
-  module.exports = errorHandlerMiddleware;
-  
